d5ebf5fc34fcff5013e24c7e4a0829eedaa742d2
generalhpba <h.p.b.a.general@hotmail.com>
2022-05-27 19:49:28 +0200

    Add intro page, fin page and song

--- script.js
+++ script.js
@@ -1,8 +1,91 @@
 document.addEventListener('DOMContentLoaded', (function () {
-    startTextLovely();
-    // startTextLoveu();
-    // startTextHeart();
+    wakeLock();
+    document.getElementById('tfPassword').addEventListener('keypress', e => {
+        if (e.key == 'Enter') {
+            onSubmit();
+        }
+    });
+    document.getElementById('container').style.display = 'flex';
+    document.body.style.background = 'url("bg.png")';
+    document.body.style.backgroundColor = 'black';
 }), false);
+
+function wakeLock() {
+    // Create the root video element
+    var video = document.createElement('video');
+    video.setAttribute('loop', '');
+    // Add some styles if needed
+    video.setAttribute('style', 'position: fixed;');
+
+    // A helper to add sources to video
+    function addSourceToVideo(element, type, dataURI) {
+        var source = document.createElement('source');
+        source.src = dataURI;
+        source.type = 'video/' + type;
+        element.appendChild(source);
+    }
+
+    // A helper to concat base64
+    var base64 = function (mimeType, base64) {
+        return 'data:' + mimeType + ';base64,' + base64;
+    };
+
+    // Add Fake sourced
+    addSourceToVideo(video, 'webm', base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
+    addSourceToVideo(video, 'mp4', base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+
+    // Append the video to where ever you need
+    document.body.appendChild(video);
+
+    // Start playing video after any user interaction.
+    // NOTE: Running video.play() handler without a user action may be blocked by browser.
+    var playFn = function () {
+        video.play();
+        document.body.removeEventListener('touchend', playFn);
+    };
+    document.body.addEventListener('touchend', playFn);
+}
+
+let cntTries = 0;
+
+function fin() {
+    document.getElementById('text-loveu').style.display = 'none';
+    document.getElementById('fin').style.display = 'block';
+    document.getElementById('fin').style.opacity = '96%';
+    document.getElementById('fin').style.animation = 'blurIn 10s ease-in forwards';
+}
+
+function onSubmit() {
+    let str = document.getElementById('tfPassword').value.toLowerCase().trim();
+    if (str == 'strawberry' || str == 'strawberries') {
+        document.documentElement.requestFullscreen();
+        document.body.style.cursor = 'none';
+        document.body.style.background = 'black';
+        document.getElementById('container').style.animation = 'fadeOut 8s forwards';
+        let audio = new Audio('think.mp3');
+        audio.volume = 0.6;
+        audio.play();
+        setTimeout(() => {
+            document.getElementById('container').style.display = 'none';
+            startTextLovely();
+        }, 10000);
+    } else {
+        let el = document.getElementById('tfPassword');
+        el.value = '';
+        el.style.animation = 'none';
+        el.offsetHeight;
+        el.style.animation = null;
+        el.style.animation = 'shake 300ms 1';
+        cntTries++;
+        if (cntTries == 1) {
+            document.getElementById('tfPassword').placeholder = "It's something red";
+        }
+        if (cntTries == 2) {
+            document.getElementById('tfPassword').placeholder = "It's something red and yummy";
+        }
+        el.focus();
+    }
+}
 
 function startTextLovely() {
     document.getElementById('text-lovely').style.display = 'flex';
@@ -24,8 +107,14 @@ function startTextLoveu() {
     setTextAnimation("svg-text-loveu", 13.2, 29, 1, 'linear', '#ff0000cc', false, fin);
 }
 
-function fin() {
-    alert("FIN");
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
 }
 
 function startPageLovely() {
@@ -36,7 +125,6 @@ function startPageLovely() {
 }
 
 function pageLovelyFinished() {
-    console.log("END ANIM LOVELY");
     document.getElementById("page-lovely").style.display = "none";
     startTextSexy();
 }
@@ -48,7 +136,6 @@ function startPageSexy() {
 }
 
 function pageSexyFinished() {
-    console.log("END ANIM SEXY");
     document.getElementById("page-sexy").style.display = "none";
     startTextHeart();
 }
@@ -61,21 +148,17 @@ function startPageHeart() {
 }
 
 function pageHeartFinished() {
-    console.log("END ANIM HEART");
     document.getElementById("page-heart").style.display = "none";
-    fin();
+    startTextLoveu();
 }
 
 function setTextAnimation(svgId, delay, duration, strokeWidth, timingFunction, strokeColor, repeat, fn) {
-    console.log('text anim');
     let paths = document.querySelectorAll("#" + svgId + " path");
-    console.log(paths.length);
     let mode = repeat ? 'infinite' : 'forwards'
 
     let calcDelay = 0;
 
     for (let i = 0; i < paths.length; i++) {
-        // console.log(paths[i]);
         const path = paths[i];
         const length = path.getTotalLength();
         path.style["stroke-dashoffset"] = `${length}px`;
@@ -83,13 +166,9 @@ function setTextAnimation(svgId, delay, duration, strokeWidth, timingFunction, s
         path.style["stroke-width"] = `${strokeWidth}px`;
         path.style["stroke"] = `${strokeColor}`;
 
-        // path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
-        // path.style["animation-delay"] = `${i * delay}s`;
-
         let dur = path.getTotalLength() * 8;
         path.style["animation"] = `${dur}ms svg-text-anim ${mode} ${timingFunction}`;
         path.style["animation-delay"] = `${calcDelay}ms`;
-        console.log(dur);
         calcDelay += dur / 2.5;
 
         if (i == paths.length - 1) {
@@ -106,4 +185,3 @@ function setTextAnimation(svgId, delay, duration, strokeWidth, timingFunction, s
         }
     }
 }
-// setTextAnimation(0.1, 13.4, 1, 'linear', '#ff0000aa', false);
--- script_sexy.js
+++ script_sexy.js
@@ -1,1 +1,145 @@
-function createSlider(e,t,r,a,o,s,n,l,i){null==n&&(n=""),null==i&&(i="");let y=document.createElement("inline");y.innerHTML=r+": "+s+n;let c=document.createElement("input");c.type="range",c.min=a,c.max=o,c.value=s,null!=l&&(c.step=l),c.oninput=(e=>{y.innerHTML=r+": "+e.target.value+n,document.querySelectorAll(t).forEach(t=>t.style.setProperty(r,e.target.value+n,i))})}function createDasharraySliders(e,t,r,a,o,s){let n=document.createElement("inline");n.innerHTML=r+": "+s;let l=[];for(let e=0;e<8;++e){let i=document.createElement("input");i.type="range",i.min=a,i.max=o,i.value=0==e?s:0,l.push(i),i.oninput=(()=>{let e="";for(let t=0;t<8&&0!=l[t].value;++t)e+=l[t].value+" ";n.innerHTML=r+": "+e,document.querySelectorAll(t).forEach(t=>t.style.setProperty(r,e)),setTotalLengths()})}}function setTotalLengths(){document.querySelectorAll("#svg-sexy path").forEach(e=>{let t=window.getComputedStyle(e).strokeDasharray.split(", "),r=0;t.forEach(e=>r+=parseInt(e.slice(0,-2))),e.style.setProperty("stroke-dashoffset",2*r);let a=Math.floor(50*Math.random())+31;e.style.setProperty("stroke-opacity",a+"%");let o=Math.floor(88*Math.random())+166,s=Math.floor(50*Math.random())+1,n=Math.floor(50*Math.random())+1;e.style.setProperty("stroke","rgba("+o+", "+s+", "+n+", 1)","important"),e.style.setProperty("stroke-dasharray",r+" "+toString(e.getTotalLength()-r))}),document.querySelectorAll("#svg-sexy2 path").forEach(e=>{let t=window.getComputedStyle(e).strokeDasharray.split(", "),r=0;t.forEach(e=>r+=parseInt(e.slice(0,-2))),e.style.setProperty("stroke-dashoffset",2*r);let a=Math.floor(50*Math.random())+31;e.style.setProperty("stroke-opacity",a+"%");let o=Math.floor(88*Math.random())+166,s=Math.floor(50*Math.random())+1,n=Math.floor(50*Math.random())+1;e.style.setProperty("stroke","rgba("+o+", "+s+", "+n+", 1)","important"),e.style.setProperty("stroke-dasharray",r+" "+toString(e.getTotalLength()-r))})}function randomDasharray(){document.querySelectorAll("#svg-sexy path").forEach(e=>{let t="";for(let e=0;e<6;++e)t+=Math.floor(Math.random()*(e%2==0?10:5)).toString(),e+1<6&&(t+=" ");e.style.setProperty("stroke-dasharray",t)}),document.querySelectorAll("#svg-sexy2 path").forEach(e=>{let t="";for(let e=0;e<6;++e)t+=Math.floor(Math.random()*(e%2==0?10:5)).toString(),e+1<6&&(t+=" ");e.style.setProperty("stroke-dasharray",t)}),setTotalLengths()}function initSexy(){setTotalLengths(),randomDasharray(),createSlider("controls-1","#svg-sexy path","stroke-miterlimit",1,100,10),createSlider("controls-2","#svg-sexy2 path","stroke-miterlimit",1,100,10),createDasharraySliders("controls-1","#svg-sexy path","stroke-dasharray",0,200,25),createDasharraySliders("controls-2","#svg-sexy2 path","stroke-dasharray",0,200,25),createSlider("controls-1","#svg-sexy path","stroke-width",0,1.5,2,"px",.01,"important"),createSlider("controls-2","#svg-sexy2 path","stroke-width",0,1.5,2,"px",.01,"important"),createSlider("controls-1","#svg-sexy path","stroke-opacity",1,100,100,"%",1,"important"),createSlider("controls-2","#svg-sexy2 path","stroke-opacity",1,100,100,"%",1,"important"),createSlider("controls-1","#svg-sexy path","animation-duration",1,2e4,1e3,"ms"),createSlider("controls-2","#svg-sexy2 path","animation-duration",1,2e4,1e3,"ms")}function animSexy(){console.log("START SEXY ANIM"),initSexy(),document.getElementById("svg-sexy").style.opacity="100%",document.getElementById("svg-sexy2").style.opacity="100%",document.querySelectorAll("#svg-sexy path").forEach(e=>{e.style.animation="\n        begin 15000ms linear forwards,\n        draw 7000ms infinite linear,\n        stop 10000ms linear 35000ms forwards"}),document.querySelectorAll("#svg-sexy2 path").forEach(e=>{e.style.animation="\n        begin 15000ms linear forwards,\n        draw 7000ms infinite linear reverse,\n        stop 10000ms linear 35000ms forwards"}),document.querySelector("#svg-sexy path").onanimationend=(e=>{"stop"==e.animationName&&pageSexyFinished()})}
+function createSlider(parentID, selector, property, min, max, value, suffix, step, isImportant) {
+    if (suffix == undefined) suffix = '';
+    if (isImportant == undefined) isImportant = '';
+
+    let label = document.createElement("inline");
+    label.innerHTML = property + ": " + value + suffix;
+
+    let slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = min;
+    slider.max = max;
+    slider.value = value;
+    if (step != undefined) slider.step = step;
+
+    slider.oninput = (e) => {
+        label.innerHTML = property + ": " + e.target.value + suffix;
+        let arr = document.querySelectorAll(selector);
+        arr.forEach(el => el.style.setProperty(property, e.target.value + suffix, isImportant));
+    };
+}
+
+function createDasharraySliders(parentID, selector, property, min, max, value) {
+    let label = document.createElement("inline");
+    label.innerHTML = property + ": " + value;
+
+    let nr = 8, sliders = [];
+    for (let i = 0; i < nr; ++i) {
+        let slider = document.createElement("input");
+        slider.type = "range";
+        slider.min = min;
+        slider.max = max;
+        if (i == 0) slider.value = value;
+        else slider.value = 0;
+        sliders.push(slider);
+        slider.oninput = () => {
+            let strVal = '';
+            for (let j = 0; j < nr; ++j) {
+                if (sliders[j].value == 0) break;
+                strVal += sliders[j].value + " ";
+            }
+            label.innerHTML = property + ": " + strVal;
+
+            let arr = document.querySelectorAll(selector);
+            arr.forEach(el => el.style.setProperty(property, strVal));
+            setTotalLengths();
+        };
+    }
+}
+
+function setTotalLengths() {
+    document.querySelectorAll("#svg-sexy path").forEach(p => {
+        let vals = window.getComputedStyle(p).strokeDasharray.split(", ");
+        let val = 0;
+        vals.forEach(v => val += parseInt(v.slice(0, -2)));
+
+        p.style.setProperty("stroke-dashoffset", val * 2);
+        let rnd = Math.floor(Math.random() * 50) + 31;
+        p.style.setProperty("stroke-opacity", rnd + "%");
+        let r = Math.floor(Math.random() * 88) + 166;
+        let g = Math.floor(Math.random() * 50) + 1;
+        let b = Math.floor(Math.random() * 50) + 1;
+        p.style.setProperty("stroke", "rgba(" + r + ", " + g + ", " + b + ", " + 1 + ")", "important");
+        p.style.setProperty("stroke-dasharray", val + " " + toString(p.getTotalLength() - val));
+    });
+
+    document.querySelectorAll("#svg-sexy2 path").forEach(p => {
+        let vals = window.getComputedStyle(p).strokeDasharray.split(", ");
+        let val = 0;
+        vals.forEach(v => val += parseInt(v.slice(0, -2)));
+
+        p.style.setProperty("stroke-dashoffset", val * 2);
+        let rnd = Math.floor(Math.random() * 50) + 31;
+        p.style.setProperty("stroke-opacity", rnd + "%");
+        let r = Math.floor(Math.random() * 88) + 166;
+        let g = Math.floor(Math.random() * 50) + 1;
+        let b = Math.floor(Math.random() * 50) + 1;
+        p.style.setProperty("stroke", "rgba(" + r + ", " + g + ", " + b + ", " + 1 + ")", "important");
+        p.style.setProperty("stroke-dasharray", val + " " + toString(p.getTotalLength() - val));
+    });
+}
+
+function randomDasharray() {
+    let limit = 10;
+    document.querySelectorAll("#svg-sexy path").forEach(p => {
+        let strarr = "";
+        for (let i = 0; i < 6; ++i) {
+            strarr += Math.floor(Math.random() * (i % 2 == 0 ? limit : limit / 2)).toString();
+            if (i + 1 < 6) strarr += " ";
+        }
+        p.style.setProperty("stroke-dasharray", strarr);
+    });
+    document.querySelectorAll("#svg-sexy2 path").forEach(p => {
+        let strarr = "";
+        for (let i = 0; i < 6; ++i) {
+            strarr += Math.floor(Math.random() * (i % 2 == 0 ? limit : limit / 2)).toString();
+            if (i + 1 < 6) strarr += " ";
+        }
+        p.style.setProperty("stroke-dasharray", strarr);
+    });
+    setTotalLengths();
+}
+
+function initSexy() {
+    setTotalLengths();
+    randomDasharray();
+
+    createSlider("controls-1", "#svg-sexy path", "stroke-miterlimit", 1, 100, 10);
+    createSlider("controls-2", "#svg-sexy2 path", "stroke-miterlimit", 1, 100, 10);
+
+    createDasharraySliders("controls-1", "#svg-sexy path", "stroke-dasharray", 0, 200, 25);
+    createDasharraySliders("controls-2", "#svg-sexy2 path", "stroke-dasharray", 0, 200, 25);
+
+    createSlider("controls-1", "#svg-sexy path", "stroke-width", 0, 1.5, 2, "px", 0.01, "important");
+    createSlider("controls-2", "#svg-sexy2 path", "stroke-width", 0, 1.5, 2, "px", 0.01, "important");
+
+    createSlider("controls-1", "#svg-sexy path", "stroke-opacity", 1, 100, 100, "%", 1, "important");
+    createSlider("controls-2", "#svg-sexy2 path", "stroke-opacity", 1, 100, 100, "%", 1, "important");
+
+    createSlider("controls-1", "#svg-sexy path", "animation-duration", 1, 20000, 1000, "ms");
+    createSlider("controls-2", "#svg-sexy2 path", "animation-duration", 1, 20000, 1000, "ms");
+}
+
+function animSexy() {
+    console.log("START SEXY ANIM");
+    initSexy();
+    document.getElementById("svg-sexy").style.opacity = '100%';
+    document.getElementById("svg-sexy2").style.opacity = '100%';
+    document.querySelectorAll("#svg-sexy path").forEach((e) => {
+        e.style.animation = `
+        begin 15000ms linear forwards,
+        draw 7000ms infinite linear,
+        stop 10000ms linear 35000ms forwards`;
+    });
+    document.querySelectorAll("#svg-sexy2 path").forEach((e) => {
+        e.style.animation = `
+        begin 15000ms linear forwards,
+        draw 7000ms infinite linear reverse,
+        stop 10000ms linear 35000ms forwards`;
+    });
+    document.querySelector("#svg-sexy path").onanimationend = ev => {
+        if (ev.animationName == 'stop') {
+            pageSexyFinished();
+        }
+    };
+}
